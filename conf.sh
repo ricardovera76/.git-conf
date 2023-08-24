147037437e8a758114e5a84c9238ede1f1a1b067
Ricardo Vera <ricardovera71@gmail.com>
2023-08-23 22:56:15 -0500

    add(prevention)

--- conf.sh
+++ conf.sh
@@ -18,4 +18,9 @@ cat $HOME/.ssh/${key_filename}.pub
 git config --global user.email "$email"
 git config --global user.name "$name"
 
+echo "Set those creds in your github account in profile>settings>SSH & GPG keys> add new SSH KEY"
+echo "This will sleep for 3 minutes, if you receive an error after that time and you created the token after that time run 'ssh -T git@github.com' t check the connection it should say 'Hi your_username'"
+
+sleep 180
+
 ssh -T git@github.com
